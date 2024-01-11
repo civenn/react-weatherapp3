f686a13f434fbd69b395ed591a7f73f968585890
civenn <141903624+civenn@users.noreply.github.com>
2024-01-11 15:27:29 +1100

    html outline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import React from "react";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <div className="container"></div>
+      <div className="container">
+        <h1> Weather app</h1>
+        <Weather />
+      </div>
     </div>
   );
 }
