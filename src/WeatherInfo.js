c316a1cffc5294cc997c6e6984899f4e9dbce5f9
civenn <141903624+civenn@users.noreply.github.com>
2024-01-19 13:42:20 +1100

    update css to align weather and icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,27 +2,34 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherConversion from "./WeatherConversion";
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
       <ul>
-        <li>
+        <li className="date">
           <FormattedDate date={props.data.date} />
         </li>
-        <li className="text-capitalize">{props.data.description}</li>
+        <li className="description">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="float-left">
-            <WeatherIcon code={props.data.icon} size={54} />
-          </div>
-          <div>
-            <WeatherConversion tempCelsius={props.data.temperature} />
+          <div className="row">
+            <div className="col-auto">
+              <div className="icon">
+                <WeatherIcon code={props.data.icon} size={54} />
+              </div>
+            </div>
+            <div className="col">
+              <div className="temperature">
+                <WeatherConversion tempCelsius={props.data.temperature} />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="col-6">
+        <div className="col-4">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
